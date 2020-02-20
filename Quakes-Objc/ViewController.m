d6cc136bc94b780bd3808aba5c56ee90024fdba5
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-20 12:41:55 -0500

    Playing with blocks

--- Quakes-Objc/ViewController.m
+++ Quakes-Objc/ViewController.m
@@ -10,6 +10,8 @@
 #import "LSILog.h"
 #import "LSIFirstResponder.h"
 
+typedef void (^WeatherCompletion)(int temperature);
+
 @interface ViewController ()
 
 @end
@@ -20,19 +22,39 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    NSLog(@"Hey Quakes!");
+//    NSLog(@"Hey Quakes!");
+//
+//    // How do I create a first responder object?
+//    LSIFirstResponder *responder = [[LSIFirstResponder alloc] init];
+//
+//    NSLog(@"responder: %@", responder);
+//    NSLog(@"name: %@", responder.name);
+//
+//    // Objects will be nil / null
+//    // int / float / double / bool will be 0 (NO)
+//
+//    responder.name = @"John";
+//    NSLog(@"name: %@", responder.name);
+//
+//
+//    // Blocks
+//    int (^addNumbers)(int a, int b) = ^int(int a, int b) {
+//        return a + b;
+//    };
+//
+//    int result = addNumbers(5, 6);
+//    NSLog(@"The sum is: %d", result);
+//    [self doWorkAndCallCompletion:^(int temperature) {
+//        NSLog(@"Finished the work, now updating the UI with %dºF", temperature);
+//    }];
+//}
+//
+//- (void)doWorkAndCallCompletion:(WeatherCompletion)completion {
+//    NSLog(@"Doing work....");
+//    completion(27);
     
-    // How do I create a first responder object?
-    LSIFirstResponder *responder = [[LSIFirstResponder alloc] init];
-
-    NSLog(@"responder: %@", responder);
-    NSLog(@"name: %@", responder.name);
     
-    // Objects will be nil / null
-    // int / float / double / bool will be 0 (NO)
     
-    responder.name = @"John";
-    NSLog(@"name: %@", responder.name);
 }
 
 
